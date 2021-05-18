439da749af4b22b1944e909095a960b486fa3d5b
David <davidbosnic98@gmail.com>
2021-05-18 17:08:16 +0200

    device service

---
+++ Projekat/SensorDeviceService/Services/Sensor.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+
+namespace SensorDeviceService.Services
+{
+    public class Sensor
+    {
+        private readonly Timer _timer;
+        private readonly StreamReader _streamReader;
+
+        public double Value { get; set; }
+        public string SensorType { get; set; }
+        public double TimerInterval { get; set; }
+        public double TresholdValue { get; set; }
+        public bool IsTresholdMeasuring { get; set; }
+
+
+        public Sensor(string type, double treshold, string path)
+        {
+            SensorType = type;
+            TresholdValue = treshold;
+            IsTresholdMeasuring = false;
+            TimerInterval = 3000;
+
+            _streamReader = new StreamReader(path);
+            _timer = new Timer(TimerInterval);
+            _timer.Elapsed += TimePased;
+        }
+
+        public void SetTimeout(double interval)
+        {
+            StopSensor();
+            TimerInterval = interval;
+            _timer.Interval = interval;
+            StartSensor();
+        }
+
+        public void StopSensor()
+        {
+            if(_timer.Enabled)
+                _timer.Stop();
+        }
+        public void StartSensor()
+        {
+            if(!_timer.Enabled)
+                _timer.Start();
+        }
+
+        public void SetTresholdValue(double treshold)
+        {
+            IsTresholdMeasuring = true;
+            TresholdValue = treshold;
+        }
+
+        private async void TimePased(object sender, ElapsedEventArgs e)
+        {
+            ReadData();
+            await SendValueAsync();
+        }
+
+        private async Task SendValueAsync()
+        {
+            if(!IsTresholdMeasuring || (IsTresholdMeasuring && (Value > TresholdValue)))
+                await PostRequest("http://sensordataservice/api/SensorData/AddSensorData");
+        }
+
+        private async Task PostRequest(string url)
+        {
+
+            Console.WriteLine(DateTime.Now.ToShortTimeString());
+            Console.WriteLine(SensorType);
+            Console.WriteLine(Value);
+
+            try
+            {
+                HttpClient httpClient = new HttpClient();
+                await httpClient.PostAsync(url, new StringContent(
+                    System.Text.Json.JsonSerializer.Serialize(new
+                    {
+                        recordTime = DateTime.Now.ToString("dd/MM/yyyy HH:mm"),
+                        type = SensorType,
+                        value = Value.ToString(),
+                        id = "",
+                    }
+                ), Encoding.UTF8, "application/json"));
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+        public void ReadData()
+        {
+            try
+            {
+                string line;
+                if (!_streamReader.EndOfStream)
+                    line = _streamReader.ReadLine();
+                else
+                {
+                    _streamReader.DiscardBufferedData();
+                    _streamReader.BaseStream.Seek(0, SeekOrigin.Begin);
+                    line = _streamReader.ReadLine();
+                }
+                Value = double.Parse(line, CultureInfo.InvariantCulture);
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
---
+++ Projekat/SensorDeviceService/Services/SensorsList.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SensorDeviceService.Services
+{
+    public class SensorsList
+    {
+        private static SensorsList sensors_list = null;
+        private static readonly object objLock = new object();
+        private List<Sensor> _sensors;
+
+        private SensorsList()
+        {
+            _sensors = new List<Sensor>();
+            _sensors.Add(new Sensor("temperature",25, "./Resources/temperature.txt"));
+            _sensors.Add(new Sensor("humidity", 50, "./Resources/humidity.txt"));
+            _sensors.Add(new Sensor("rainfall", 0.5, "./Resources/rainfall.txt"));
+            _sensors.Add(new Sensor("pressure", 1013, "./Resources/pressure.txt"));
+            _sensors.Add(new Sensor("windspeed", 15, "./Resources/windspeed.txt"));
+        }
+
+        public List<Sensor> GetSensors()
+        {
+            if (sensors_list == null)
+                sensors_list = new SensorsList();
+            return _sensors;
+        }
+
+        public static SensorsList GetSensorsListInstance()
+        {
+            if (sensors_list == null)
+            {
+                lock (objLock)
+                {
+                    if (sensors_list == null)
+                    {
+                        sensors_list = new SensorsList();
+                    }
+                }
+            }
+
+            return sensors_list;
+        }
+    }
+}
